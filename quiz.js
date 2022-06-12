c7133b0f66f3a8f0730692c4eaefc2d89769c7fa
SanyaOdare <sanyaodare@gmail.com>
2022-06-12 21:08:56 +0100

    work more on script for choices

--- quiz.js
+++ quiz.js
@@ -2,7 +2,7 @@ let question = document.getElementById('question');
 const choices = Array.from(document.getElementsByClassName('choice-text'));
 
 let currentQuestion = {};
-let acceptingAnswers = true;
+let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
@@ -47,6 +47,15 @@ startQuiz = () => {
 }
 
 getNewQuestion = () => {
+  // if no more question 
+  if(availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+    // go to end of page
+    return window.location.assign("/end.html");
+
+  }
+  
+  
+  
   questionCounter++;
   const questionIndex = Math.floor(Math.random() * availableQuestions.length);
   currentQuestion = availableQuestions[questionIndex];
@@ -56,6 +65,22 @@ getNewQuestion = () => {
     const number = choice.dataset['number'];
     choice.innerText = currentQuestion['choice' + number];
   })
+
+  availableQuestions.splice(questionIndex, 1);
+  acceptingAnswers = true;
 };
+
+choices.forEach(choice => {
+  choice.addEventListener('click', e => {
+    // if no answer is clicked
+    if(!acceptingAnswers) return;
+
+    acceptingAnswers = false;
+    const selectedChoice = e.target;
+    const selectedAnswer = selectedChoice.dataset['number'];
+    console.log(selectedAnswer);    
+    getNewQuestion();
+  });
+});
 
 startQuiz();
