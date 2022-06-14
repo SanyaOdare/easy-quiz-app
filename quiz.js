5bc055d041fcaa589d09c2f1f3dd547a7d673679
SanyaOdare <sanyaodare@gmail.com>
2022-06-14 08:45:07 +0100

    create a json file and test

--- quiz.js
+++ quiz.js
@@ -10,32 +10,32 @@ let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 
-let questions = [
-  {
-    question: "Who delivered most inspiring speech at APC's Primaries?",
-    choice1: "Osinbajo",
-    choice2: "Tinubu",
-    choice3: "Atiku",
-    choice4: "Buhari",
-    answer: 1
-  },
-  {
-    question: "Which rock band was founded by Trent Reznor in 1988?",
-    choice1: "Mamma Mia by Abba",
-    choice2: "Coldplay",
-    choice3: "Nine Inch Nails",
-    choice4: "Joni Mitchell",
-    answer: 3
-  },
-  {
-    question: "Which English Sir has had No. l’s in the 50’s, 60’s, 70’s, 80’s and 90’s?",
-    choice1: "Led Zeppelin",
-    choice2: "Elton John",
-    choice3: "Britney Spears",
-    choice4: "Sir Cliff Richard",
-    answer: 4
-  },
-]
+// let questions = [
+//   {
+//     question: "Who delivered most inspiring speech at APC's Primaries?",
+//     choice1: "Osinbajo",
+//     choice2: "Tinubu",
+//     choice3: "Atiku",
+//     choice4: "Buhari",
+//     answer: 1
+//   },
+//   {
+//     question: "Which rock band was founded by Trent Reznor in 1988?",
+//     choice1: "Mamma Mia by Abba",
+//     choice2: "Coldplay",
+//     choice3: "Nine Inch Nails",
+//     choice4: "Joni Mitchell",
+//     answer: 3
+//   },
+//   {
+//     question: "Which English Sir has had No. l’s in the 50’s, 60’s, 70’s, 80’s and 90’s?",
+//     choice1: "Led Zeppelin",
+//     choice2: "Elton John",
+//     choice3: "Britney Spears",
+//     choice4: "Sir Cliff Richard",
+//     answer: 4
+//   }
+// ]
 
 // CONSTANTS
 const CORRECT_BONUS = 10;
